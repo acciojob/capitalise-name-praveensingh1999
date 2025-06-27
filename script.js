205458c92b3bd8544775ce9c8cce667eebb255a9
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-06-27 13:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const inputField = document.getElementById("fname");
+
+    // Add blur event listener to convert text to uppercase
+    inputField.addEventListener("blur", function () {
+      inputField.value = inputField.value.toUpperCase();
